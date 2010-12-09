fc422d58d66ad408e37eeb198a2bd58e4e1700a6
Leonid Mamchenkov <leonid@mamchenkov.net>
2010-12-09 17:46:40 +0200

    Big refactoring

--- torrents.php
+++ torrents.php
@@ -1,14 +1,14 @@
 <?php
 	define('FILE_SHOWS', dirname(__FILE__) . '/shows.txt');
+	define('FILE_HISTORY', dirname(__FILE__) . '/history.log');
 
 	// Path to download torrrent files to
-	define('AUTOTORRENTS_PATH',"");
+	define('AUTOTORRENTS_PATH','torrents/');
 	// RSS Feed of torrents (based on torrentleech.org)
-	$url = "http://rss.torrentleech.org/KEY";
+	//$url = "http://rss.torrentleech.org/KEY";
+	$url = "http://www.torrentz.com/feed";
 	// Exlude these matches
 	$exclude = "(720p|brrip|1080p|dvdrip|hebsub|repack|dvdr)";
-	// File to keep history of downloaded torrrents
-	$historyFile = AUTOTORRENTS_PATH."history.txt";
 
 	/**
 	 * Get the list of shows to download
@@ -33,65 +33,109 @@
 		return $result;
 	}
 
-	$shows = getShows();
+	function downloadFeed($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
+		curl_close($ch);
 
-	$downloadList = array();
+		return $result;	
+	}	
 
-	
-	function curl($file,$download=0){
-		if ($download){
-			echo "Downloading: ". AUTOTORRENTS_PATH.basename($file). "\n";
-			$ch = curl_init($file);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
-			$fp = fopen(AUTOTORRENTS_PATH.basename($file), 'w+');
-			curl_setopt($ch, CURLOPT_FILE, $fp);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_exec($ch);
-			curl_close($ch);
-			fclose($fp);
-		} else {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
-    		curl_setopt($ch, CURLOPT_URL, $file);
-		    curl_setopt($ch, CURLOPT_HEADER, 0);
-		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		    curl_setopt($ch, CURLOPT_TIMEOUT, 500);	
-		    $output = curl_exec($ch);
-    		curl_close($ch);
-    		return $output;	
+	function downloadTorrent($url){
+		$fileName = basename($url);
+		if (!preg_match('/\.torrent/', $fileName)) {
+			$fileName .= '.torrent';
 		}
+
+		if (!file_exists(AUTOTORRENTS_PATH)) {
+			mkdir(AUTOTORRENTS_PATH);
+		}
+
+		if (file_exists(AUTOTORRENTS_PATH) && is_dir(AUTOTORRENTS_PATH) && is_writable(AUTOTORRENTS_PATH)) {
+			$fileName = AUTOTORRENTS_PATH . $fileName;
+		}
+
+		echo "Downloading: ". $fileName . "\n";
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
+		$fp = fopen($fileName, 'w+');
+		curl_setopt($ch, CURLOPT_FILE, $fp);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_exec($ch);
+		curl_close($ch);
+		fclose($fp);
 	}
 
-	
-	$xml = simplexml_load_string(curl($url),"SimpleXMLElement",LIBXML_NOCDATA);
-//	$xml = simplexml_load_string(file_get_contents('test.xml'),"SimpleXMLElement",LIBXML_NOCDATA);
-	
+	function getFeedItems($url) {
+		$result = array();
 
-	foreach($xml->channel->item as $i){
-		$torrent = $i->link;
-		$title = $i->title;
-		
-		if (!preg_match("/$exclude/is",$title)){ 
-			if (preg_match("/$shows\s(.*?)S([0-9]+?)E([0-9]+?)\s/is",$title,$m)){
+		libxml_use_internal_errors(true);
+		$xml = simplexml_load_string(downloadFeed($url));
+		if ($xml) {
+			foreach ($xml->channel->item as $item) {
+				$link = ($item->link) ? $item->link : $item->guid;
+				$result[ (string) $link ] = (string) $item->title;
+			}
+		}
+
+		return $result;
+	}
+
+	function cleanFeedItems($feedItems, $shows, $exclude) {
+		$result = array();
+
+		foreach ($feedItems as $link => $title) {
+			// Ignore exludes
+			if (preg_match("/$exclude/is", $title)) {
+				continue;
+			}
+
+			if (preg_match("/$shows\s(.*?)S([0-9]+?)E([0-9]+?)\s/is", $title, $m)){
 				$episode = "S".$m[3]."E".$m[4];
-				preg_match("/(.*?)$episode(.*?)/is",$title,$cleanTitle);
-				$downloadList[] = array(ucfirst(trim($cleanTitle[1])),$torrent,$episode);
+				preg_match("/(.*?)$episode(.*?)/is", $title, $cleanTitle);
+				$result[] = array(
+					'show' => ucfirst(trim($cleanTitle[1])),
+					'url' => $link,
+					'episode' => $episode,
+				);
 			}			
 		}
+
+		return $result;
 	}
+
+	function getHistory() {
+		$result = array();
+
+		if (is_file(FILE_HISTORY) && file_exists(FILE_HISTORY) && is_readable(FILE_HISTORY)) {
+			$result = file(FILE_HISTORY, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		}
+
+		return $result;
+	}
+
+	function saveHistory($history) {
+		return file_put_contents(FILE_HISTORY, implode("\n", $history));
+	}
+
+	$shows = getShows();
+
+	$feedItems = getFeedItems($url);
+	$cleanItems = cleanFeedItems($feedItems, $shows, $exclude);
+	$history = getHistory();
 	
-	//print_r($downloadList);exit;
-	$lines = file($historyFile,FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	foreach($downloadList as $d){
-		$entry = $d[0]." ".$d[2];
-		$download = true;
-		if (in_array($entry,$lines)) $download = false;
-		if ($download) {	
-			curl($d[1][0],true);
-			$history = fopen ($historyFile, 'a');
-			fwrite($history,$entry."\n");
-			fclose($history);
+	foreach($cleanItems as $d){
+		$entry = $d['show']." ".$d['episode'];
+		if (!in_array($entry,$history)) {
+			downloadTorrent($d['url']);
+			$history[] = $entry;
 		}
 	}
+
+	saveHistory($history);
 	
 ?>
